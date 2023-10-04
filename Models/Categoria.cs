d08157fb745a796c430bec8b85e3284701411e0d
Gustavo Amorim <gustavoamorilsilva@gmail.com>
2023-10-04 02:24:35 -0300

    Adicionando models Categoria e Lanche

---
+++ Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace NetBurguer.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
+}
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NetBurguer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
