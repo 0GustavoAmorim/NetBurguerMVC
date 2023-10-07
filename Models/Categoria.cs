e3d99b17ff9c423dc86c4ae30b52fcde85576509
Gustavo Amorim <gustavoamorilsilva@gmail.com>
2023-10-06 21:01:47 -0300

    Aplicando pacotes e ApplicationDbContext

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -5,4 +5,6 @@ public class Categoria
     public int CategoriaId { get; set; }
     public string CategoriaNome { get; set; }
     public string Descricao { get; set; }
+
+    public List<Lanche> Lanches { get; set; }
 }
